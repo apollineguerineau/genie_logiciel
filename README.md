68aa72afa84a930d2d834e81a3182dcc728d4f3e
apollineguerineau <apolline.guerineau@eleve.ensai.fr>
2024-02-09 15:07:42 +0100

    API avec aws

--- README.md
+++ README.md
@@ -5,7 +5,15 @@ mvn compile
 mvn package
 java -cp target/demo-1.0.jar fr.ensai.demo.Main
 
+# changer les credentials pour aws
+aws configure
+aws configure set aws_session_token "<token>"
+
+# upload un dossier dans un bucket S3 (pour tester)
+aws s3 sync dossier_a_upload s3://bucketprojectapolline/dossier1
+
 # pour run l'application :
 mvn spring-boot:run
 
 http://localhost:8080/localfilesystem
+http://localhost:8080/S3
--- src/main/java/fr/ensai/demo/controller/HelloWorldController.java
+++ src/main/java/fr/ensai/demo/controller/HelloWorldController.java
@@ -9,6 +9,11 @@ import fr.ensai.demo.model.filesystem.FileLeaf;
 import fr.ensai.demo.model.filesystem.FolderComponent;
 import fr.ensai.demo.model.strategy.LocalFileSystemScanner;
 import fr.ensai.demo.model.visitor.FileSystemVisitor;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.ListObjectsV2Request;
+import software.amazon.awssdk.services.s3.model.ListObjectsV2Response;
+import software.amazon.awssdk.services.s3.model.S3Object;
 
 import java.util.List;
 
@@ -48,6 +53,38 @@ public class HelloWorldController {
         tableBuilder.append("</table>");
 
         return tableBuilder.toString();
+    }
+
+    @GetMapping("/S3")
+    public String getS3FileSystem() {
+        String bucketName = "bucketprojectapolline";
+
+        final S3Client s3 = S3Client.builder().region(Region.US_EAST_1).build();
+        ListObjectsV2Response response = s3.listObjectsV2(ListObjectsV2Request.builder().bucket(bucketName).build());
+
+        StringBuilder tableBuilder = new StringBuilder();
+        tableBuilder.append("<table border=\"1\">");
+        tableBuilder.append("<tr><th>Name</th><th>File Extension</th><th>Parent Folder Name</th><th>Modification Date</th><th>Size</th></tr>");
+
+        for (S3Object object : response.contents()) {
+            String fileName = object.key();
+            String lastModified = object.lastModified().toString();
+            long size = object.size();
+            String fileType = fileName.substring(fileName.lastIndexOf('.') + 1);
+            String directory = fileName.contains("/") ? fileName.substring(0, fileName.lastIndexOf('/')) : "";
+
+            tableBuilder.append("<tr>");
+            tableBuilder.append("<td>").append(fileName).append("</td>");
+            tableBuilder.append("<td>").append(fileType).append("</td>");
+            tableBuilder.append("<td>").append(directory).append("</td>");
+            tableBuilder.append("<td>").append(lastModified).append("</td>");
+            tableBuilder.append("<td>").append(size).append("</td>");
+            tableBuilder.append("</tr>");
+        }
+        tableBuilder.append("</table>");
+
+        return tableBuilder.toString();
+
     }
 }
 
